5c41337a3375400f0c1b63bb240cfbee0211fb88
Harrison Royds <harrisonroyds@Harrisons-MacBook-Pro.local>
2022-10-31 17:06:01 +0000

    got to task 6

---
+++ regex.js
@@ -0,0 +1,2 @@
+console.log(/\d+$/.test("hello1"));
+console.log(/\d+$/.test("1hello"));
--- server.js
+++ server.js
@@ -3,13 +3,14 @@ const fs = require("fs/promises");
 
 const server = http.createServer((request, response) => {
   const { method, url } = request;
-
+  //TASK 1
   if (method === "GET" && url === "/api") {
     response.setHeader("Content-Type", "application/json");
     response.statusCode = 200;
     response.write(JSON.stringify({ message: "Hello!" }));
     response.end();
   }
+  //TASK 2
   if (method === "GET" && url === "/api/books") {
     fs.readFile("./data/books.json", "utf8").then((books) => {
       const bookJS = JSON.parse(books);
@@ -20,6 +21,7 @@ const server = http.createServer((request, response) => {
       response.end();
     });
   }
+  //TASK 3
   if (method === "GET" && url === "/api/authors") {
     fs.readFile("./data/authors.json", "utf8").then((authors) => {
       const authorsJS = JSON.parse(authors);
@@ -30,7 +32,60 @@ const server = http.createServer((request, response) => {
       response.end();
     });
   }
+  //TASK 4
+  if (method === "GET" && /\/api\/books\/\d+$/.test(url)) {
+    fs.readFile("./data/books.json", "utf8").then((books) => {
+      const booksJS = JSON.parse(books);
+
+      const lastDigit = url.split("/")[3];
+      const filteredBooks = booksJS.filter((book) => {
+        return book.bookId === +lastDigit;
+      });
+      const bookResponse = filteredBooks;
+      response.setHeader("Content-Type", "application/json");
+      response.statusCode = 200;
+      response.write(JSON.stringify(bookResponse));
+      response.end();
+    });
+  }
+  //TASK 5
+  if (method === "POST" && url === "/api/books") {
+    let body = "";
+    request.on("data", (packet) => {
+      body += packet.toString();
+    });
+    request.on("end", () => {
+      const parsedBody = JSON.parse(body);
+      fs.readFile("./data/books.json", "utf8")
+        .then((books) => {
+          const booksJS = JSON.parse(books);
+          const highestSoFar = booksJS.length;
+          parsedBody.bookId = highestSoFar + 1;
+          booksJS.push(parsedBody);
+          return fs.writeFile(
+            "./data/books.json",
+            JSON.stringify(booksJS, null, 2)
+          );
+        })
+        .then(() => {
+          response.setHeader("Content-Type", "application/json");
+          response.statusCode = 201;
+          response.write(JSON.stringify(parsedBody));
+          response.end();
+        })
+        .catch((err) => {
+          if (err) {
+            console.log(err);
+          }
+        });
+    });
+  }
+  //TASK 6
+  if (method === "GET" && /\/api\/books\/\d+\/author$/.test(url)) {
+    console.log("task 6");
+  }
 });
+
 server.listen(8080, (err) => {
   if (err) {
     console.log(err);
